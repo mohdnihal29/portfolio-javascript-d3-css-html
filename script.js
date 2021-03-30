38393540e3dec9ffe1212a661f10b205a4b9001d
mohdnihal29 <mohdnihal29@gmail.com>
2021-03-30 17:47:20 +1100

    Final commit

--- script.js
+++ script.js
@@ -11,32 +11,6 @@ const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 
-///////////////////////////////////////
-// Modal window
-
-// const openModal = function (e) {
-//   e.preventDefault();
-//   modal.classList.remove('hidden');
-//   overlay.classList.remove('hidden');
-// };
-
-// const closeModal = function () {
-//   modal.classList.add('hidden');
-//   overlay.classList.add('hidden');
-// };
-
-// btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
-
-// btnCloseModal.addEventListener('click', closeModal);
-// overlay.addEventListener('click', closeModal);
-
-// document.addEventListener('keydown', function (e) {
-//   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-//     closeModal();
-//   }
-// });
-
-///////////////////////////////////////
 // Button scrolling
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
@@ -176,12 +150,3 @@ const imgObserver = new IntersectionObserver(loadImg, {
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
-
-//////////////////////////////////// 2
-//////////////////////////////////// 2
-//////////////////////////////////// 2
-//////////////////////////////////// 2
-//////////////////////////////////// 2
-//////////////////////////////////// 2
-//////////////////////////////////// 2
-//////////////////////////////////// 2
